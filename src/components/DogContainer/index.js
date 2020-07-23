51c2b50f4e4312391c41c7d8a04e05ea84ab3f66
JamesLee <48775473+titanian229@users.noreply.github.com>
2020-07-23 12:30:08 -0400

    Moved fetching of random dog one container down so title didn't need to refresh

--- src/components/DogContainer/index.js
+++ src/components/DogContainer/index.js
@@ -2,18 +2,36 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 // import styles from './styles.scss';
 import LikeButton from '../LikeButton'
+import useAxios from 'axios-hooks';
+import Loading from '../../components/Loading'
+import Alert from 'react-bootstrap/Alert';
+
 
 const DogContainer = (props) => {
-    const source = props.dog.message;
 
+    const [{ data, loading, error }, refetch] = useAxios('https://dog.ceo/api/breeds/image/random');
+
+    if (loading) return <Loading/>;
+    if (error) return <Alert variant="danger">Error!</Alert>;
+
+    const source = data.message
+
+    const likeClicked = () => {
+        props.likeClicked()
+        refetch()
+    }
+    const dislikeClicked = () => {
+        props.dislikeClicked()
+        refetch()
+    }
 
 
     return (
         <Card style={{ width: '30vw', height: '30vw', backgroundImage: "url(" + source + ")", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center center", position: "relative" }}>
             {/* <Image rounded src={source} fluid /> */}
                 <Card.Text>
-                    <LikeButton likeClicked={props.likeClicked} style={{position: "absolute", left: "1em", bottom: "1em", fontSize: "3em", color: "green"}} up={true} />
-                    <LikeButton dislikeClicked={props.dislikeClicked}  style={{position: "absolute", right: "1em", bottom: "1em", fontSize: "3em", color: "red"}} up={false} />
+                    <LikeButton likeClicked={likeClicked} style={{position: "absolute", left: "1em", bottom: "1em", fontSize: "3em", color: "green"}} up={true} />
+                    <LikeButton dislikeClicked={dislikeClicked}  style={{position: "absolute", right: "1em", bottom: "1em", fontSize: "3em", color: "red"}} up={false} />
                 </Card.Text>
         </Card>
     );
--- src/pages/Discover/index.js
+++ src/pages/Discover/index.js
@@ -1,42 +1,33 @@
 import React, { useState } from 'react';
-import useAxios from 'axios-hooks';
 import Alert from 'react-bootstrap/Alert';
 import DogContainer from '../../components/DogContainer';
-import Loading from '../../components/Loading'
 
 const Discover = () => {
-    const [{ data, loading, error }, refetch] = useAxios('https://dog.ceo/api/breeds/image/random');
-
     if (!localStorage.dogsLiked) {
-        localStorage.dogsLiked = 0
+        localStorage.dogsLiked = 0;
     }
 
-    const dogsLikedSaved = Number(localStorage.dogsLiked)
+    const dogsLikedSaved = Number(localStorage.dogsLiked);
 
     const [dogsMutuallyLiked, setDogsMutuallyLiked] = useState(dogsLikedSaved);
-    const [lastLike, setLastLike] = useState(false)
+    const [lastLike, setLastLike] = useState(false);
 
     const likeClicked = (clickVariant) => {
         if (Math.random() > 0.5) {
-            localStorage.dogsLiked = dogsMutuallyLiked + 1
-            setDogsMutuallyLiked(dogsMutuallyLiked+1)
-            setLastLike(true)
+            localStorage.dogsLiked = dogsMutuallyLiked + 1;
+            setDogsMutuallyLiked(dogsMutuallyLiked + 1);
+            setLastLike(true);
         } else {
-            setLastLike(false)
+            setLastLike(false);
         }
-        refetch();
     };
 
     const dislikeClicked = () => {
-        console.log('disliked')
-        window.alert("They're good dogs, what's wrong with you?")
-        setLastLike(false)
-        refetch()
-    }
+        console.log('disliked');
+        window.alert("They're good dogs, what's wrong with you?");
+        setLastLike(false);
+    };
 
-
-    if (loading) return <Loading />;
-    if (error) return <Alert variant="danger">Error!</Alert>;
     return (
         <div>
             <h1 className="text-center">Meet new dogs!</h1>
@@ -44,9 +35,17 @@ const Discover = () => {
                 Thumbs up any dog you'd like to meet, and thumbs down if you're a literal monster
             </h2>
             <div className="d-flex flex-column align-items-center justify-content-center">
-                <DogContainer dog={data} likeClicked={likeClicked} dislikeClicked={dislikeClicked} />
-                {(lastLike) ? <Alert variant="success" className="mt-2">That dog liked you too!</Alert> : ''}
-                <h2>You've made {dogsMutuallyLiked} {(dogsMutuallyLiked === 1) ? 'friend' : 'friends'} so far</h2>
+                <DogContainer likeClicked={likeClicked} dislikeClicked={dislikeClicked} />
+                {lastLike ? (
+                    <Alert variant="success" className="mt-2">
+                        That dog liked you too!
+                    </Alert>
+                ) : (
+                    ''
+                )}
+                <h2>
+                    You've made {dogsMutuallyLiked} {dogsMutuallyLiked === 1 ? 'friend' : 'friends'} so far
+                </h2>
             </div>
         </div>
     );
